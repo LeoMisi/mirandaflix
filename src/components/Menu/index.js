a0c02f890f97d98f106010996a85bf48b50acd54
Leonardo Silva <leonardo.silva@campinas.sottelli.com.br>
2020-07-30 22:51:38 -0300

    Second Class Imersao React

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
-// import ButtonLink from './components/ButtonLink'
 import Button from '../Button'
 
 function Menu(){
   return (
     <nav className="Menu">
-      <a href="/">
-      <img className="Logo" src={Logo} alt="MirandaFlix"></img>
-      </a>
+      <Link to="/">
+      <img className="Logo" src={Logo} alt="MirandaFlix"/>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
